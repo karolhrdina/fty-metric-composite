f578ef4b59e9b21668486b025aae1bb7be409325
Michal Vyskocil <michalvyskocil@eaton.com>
2015-12-04 10:44:28 +0100

    Problem: can't test composite metrics
    Solution: rewrite it to actor add _test for it
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

---
+++ include/bios_composite_metrics_server.h
@@ -0,0 +1,30 @@
+/*
+Copyright (C) 2014 - 2015 Eaton
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <czmq.h>
+
+#ifndef _BIOS_LM_SERVER_H_
+#define _BIOS_LM_SERVER_H_
+
+void
+bios_composite_metrics_server (zsock_t *pipe, void* args);
+
+void
+bios_composite_metrics_server_test (bool verbose);
+
+#endif
--- src/composite-metrics.cc
+++ src/composite-metrics.cc
@@ -15,109 +15,35 @@ extern "C" {
 #include <bios_proto.h>
 #include <malamute.h>
 
-struct value {
-  double value;
-  time_t valid_till;
-};
-
-std::map<std::string, value> cache;
+#include "composite_metrics.h"
 
 int main (int argc, char** argv) {
-  std::string lua_code;
 
-  // Read configuration
-  if(argc < 2) {
-    printf("Syntax: %s config\n", argv[0]);
-  }
-  mlm_client_t *client = mlm_client_new();
-  mlm_client_connect(client, "ipc://@/malamute", 1000, argv[1]);
-  mlm_client_set_producer(client, "METRICS");
-  std::ifstream f(argv[1]);
-  cxxtools::JsonDeserializer json(f);
-  json.deserialize();
-  const cxxtools::SerializationInfo *si = json.si();
-  si->getMember("evaluation") >>= lua_code;
-
-  // Subscribe to all streams
-  for(auto it : si->getMember("in")) {
-    std::string buff;
-    it >>= buff;
-    mlm_client_set_consumer(client, "METRICS", buff.c_str());
-    printf("Registered to receive '%s'\n", buff.c_str());
-  }
-
-
-  while(!zsys_interrupted) {
-    // Get message
-    zmsg_t *msg = mlm_client_recv(client);
-    if(msg == NULL)
-      continue;
-    bios_proto_t *yn = bios_proto_decode(&msg);
-    if(yn == NULL)
-      continue;
-
-    // Update cache with updated values
-    std::string topic = mlm_client_subject(client);
-    value val;
-    val.value = atof(bios_proto_value(yn));
-    val.valid_till = time(NULL) + 600;
-    printf("Got message '%s' with value %lf\n", topic.c_str(), val.value);
-    auto f = cache.find(topic);
-    if(f != cache.end()) {
-      f->second = val;
-    } else {
-      cache.insert(std::make_pair(topic, val));
+    // Read configuration
+    if(argc < 2) {
+        printf("Syntax: %s config\n", argv[0]);
     }
-    bios_proto_destroy(&yn);
 
-    // Prepare data for computation
-#if LUA_VERSION_NUM > 501
-    lua_State *L = luaL_newstate();
-#else
-    lua_State *L = lua_open();
-#endif
-    luaL_openlibs(L);
-    lua_newtable(L);
-    time_t tme = time(NULL);
-    for(auto i : cache) {
-      if(tme > i.second.valid_till)
-        continue;
-      printf(" - %s, %f\n", i.first.c_str(), i.second.value);
-      lua_pushstring(L, i.first.c_str());
-      lua_pushnumber(L, i.second.value);
-      lua_settable(L, -3);
-    }
-    lua_setglobal(L, "mt");
+    zactor_t *cm_server = zactor_new (bios_composite_metrics_server, (void*) "composite-metrics");
+    zstr_sendx (cm_server, "CONNECT", "ipc://@/malamute", NULL);
+    zstr_sendx (cm_server, "PRODUCER", "METRICS", NULL);
+    zstr_sendx (cm_server, "CONFIG", argv[1], NULL);
+    zclock_sleep (500);  // to settle down the things
 
-    // Do the real processing
-    auto error = luaL_loadbuffer(L, lua_code.c_str(), lua_code.length(), "line") ||
-                 lua_pcall(L, 0, 3, 0);
-    if(error) {
-      fprintf(stderr, "%s", lua_tostring(L, -1));
-      goto next_iter;
+    //  Accept and print any message back from server
+    //  copy from src/malamute.c under MPL license
+    while (true) {
+        char *message = zstr_recv (cm_server);
+        if (message) {
+            puts (message);
+            free (message);
+        }
+        else {
+            puts ("interrupted");
+            break;
+        }
     }
-    printf("Total: %d\n", lua_gettop(L));
-    if(lua_gettop(L) == 3) {
-         if(strrchr(lua_tostring(L, -3), '@') == NULL) {
-           printf("Invalid output topic\n");
-           goto next_iter;
-         }
-         bios_proto_t *n_met = bios_proto_new(BIOS_PROTO_METRIC);
-         char *buff = strdup(lua_tostring(L, -3));
-         bios_proto_set_element_src(n_met, "%s", strrchr(buff, '@') + 1);
-         (*strrchr(buff, '@')) = 0;
-         bios_proto_set_type(n_met, "%s", buff);
-         bios_proto_set_value(n_met, "%s", lua_tostring(L, -2));
-         bios_proto_set_unit(n_met,  "%s", lua_tostring(L, -1));
-         bios_proto_set_time(n_met,  -1);
-         zmsg_t* z_met = bios_proto_encode(&n_met);
-         mlm_client_send(client, lua_tostring(L, -3), &z_met);
-    } else {
-       fprintf(stdout, "Not enough valid data...\n");
-    }
-next_iter:
-    lua_close(L);
-  }
-  mlm_client_destroy(&client);
-  return 0;
+
+    zactor_destroy (&cm_server);
+    return 0;
 }
