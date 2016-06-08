75441056572f0cab09d255617ab6b9a5c8b712ec
Karol Hrdina <karolhrdina@eaton.com>
2016-06-08 13:07:37 +0200

    PROBLEM: Project does not have class for logger/actor_commands/data SOLUTION: add it
    PROBLEM: Re-generated project's build directory got replaced by
    packaging
    SOLUTION: merge the solutions
    
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

---
+++ src/actor_commands.cc
@@ -0,0 +1,413 @@
+/*  =========================================================================
+    actor_commands - actor commands
+
+    Copyright (C) 2014 - 2015 Eaton                                        
+                                                                           
+    This program is free software; you can redistribute it and/or modify   
+    it under the terms of the GNU General Public License as published by   
+    the Free Software Foundation; either version 2 of the License, or      
+    (at your option) any later version.                                    
+                                                                           
+    This program is distributed in the hope that it will be useful,        
+    but WITHOUT ANY WARRANTY; without even the implied warranty of         
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+    GNU General Public License for more details.                           
+                                                                           
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    =========================================================================
+*/
+
+/*
+@header
+    actor_commands - actor commands
+@discuss
+@end
+*/
+
+#include "composite_metrics_classes.h"
+
+int
+actor_commands (
+        mlm_client_t *client,
+        zmsg_t **message_p) {
+/*
+    assert (message_p && *message_p);
+    assert (data);
+    assert (fullpath);
+    zmsg_t *message = *message_p;
+    
+    char *cmd = zmsg_popstr (message);
+    if (!cmd) {
+        log_error (
+                "Given `which == pipe` function `zmsg_popstr (msg)` returned NULL. "
+                "Message received is most probably empty (has no frames).");
+        zmsg_destroy (message_p);
+        return 0;
+    }
+
+    int ret = 0;
+    log_debug ("actor command = '%s'", cmd);
+    if (streq (cmd, "$TERM")) {
+        ret = 1;
+    }
+    else
+    if (streq (cmd, "CONNECT")) {
+        char *endpoint = zmsg_popstr (message);
+        if (!endpoint) {
+            log_error (
+                    "Expected multipart string format: CONNECT/endpoint/name. "
+                    "Received CONNECT/nullptr");
+            zstr_free (&cmd);
+            zmsg_destroy (message_p);
+            return 0;
+        }
+        char *name = zmsg_popstr (message);
+        if (!name) {
+            log_error (
+                    "Expected multipart string format: CONNECT/endpoint/name. "
+                    "Received CONNECT/endpoint/nullptr");
+            zstr_free (&endpoint);
+            zstr_free (&cmd);
+            zmsg_destroy (message_p);
+            return 0;
+        }
+        int rv = mlm_client_connect (client, endpoint, 1000, name);
+        if (rv == -1) {
+            log_error (
+                    "mlm_client_connect (endpoint = '%s', timeout = '%d', address = '%s') failed",
+                    endpoint, 1000, name);
+        }
+
+        zstr_free (&endpoint);
+        zstr_free (&name);
+    }
+    else
+    if (streq (cmd, "PRODUCER")) {
+        char *stream = zmsg_popstr (message);
+        if (!stream) {
+            log_error (
+                    "Expected multipart string format: PRODUCER/stream. "
+                    "Received PRODUCER/nullptr");
+            zstr_free (&stream);
+            zstr_free (&cmd);
+            zmsg_destroy (message_p);
+            return 0;
+        }
+        int rv = mlm_client_set_producer (client, stream);
+        if (rv == -1) {
+            log_error ("mlm_client_set_producer (stream = '%s') failed", stream);
+        }
+        zstr_free (&stream);
+    }
+    else
+    if (streq (cmd, "CONSUMER")) {
+        char *stream = zmsg_popstr (message);
+        if (!stream) {
+            log_error (
+                    "Expected multipart string format: CONSUMER/stream/pattern. "
+                    "Received CONSUMER/nullptr");
+            zstr_free (&cmd);
+            zmsg_destroy (message_p);
+            return 0;
+        }
+        char *pattern = zmsg_popstr (message);
+        if (!pattern) {
+            log_error (
+                    "Expected multipart string format: CONSUMER/stream/pattern. "
+                    "Received CONSUMER/stream/nullptr");
+            zstr_free (&stream);
+            zstr_free (&cmd);
+            zmsg_destroy (message_p);
+            return 0;
+        }
+        int rv = mlm_client_set_consumer (client, stream, pattern);
+        if (rv == -1) {
+            log_error (
+                    "mlm_client_set_consumer (stream = '%s', pattern = '%s') failed",
+                    stream, pattern);
+        }
+        zstr_free (&pattern);
+        zstr_free (&stream);
+    }
+    else
+    if (streq (cmd, "CONFIGURE")) {
+        char *state_file = zmsg_popstr (message);
+        if (!state_file) {
+            log_error (
+                    "Expected multipart string format: CONFIGURE/state_file."
+                    "Received CONFIGURE/nullptr");
+            zstr_free (&cmd);
+            zmsg_destroy (message_p);
+            return 0;
+        }
+        *fullpath = strdup (state_file);
+        rt_load (data, state_file);
+        zstr_free (&state_file); 
+    }
+    else {
+        log_warning ("Command '%s' is unknown or not implemented", cmd);
+    }
+
+    zstr_free (&cmd);
+    zmsg_destroy (message_p);
+    return ret;
+*/
+    return 0;
+}
+
+
+//  --------------------------------------------------------------------------
+//  Self test of this class
+
+#define STDERR_EMPTY \
+    {\
+    fseek (fp, 0L, SEEK_END);\
+    uint64_t sz = ftell (fp);\
+    fclose (fp);\
+    assert (sz == 0);\
+    }
+
+#define STDERR_NON_EMPTY \
+    {\
+    fseek (fp, 0L, SEEK_END);\
+    uint64_t sz = ftell (fp);\
+    fclose (fp);\
+    assert (sz > 0);\
+    }
+
+void
+actor_commands_test (bool verbose)
+{
+    printf (" * actor_commands: ");/*
+    //  @selftest
+    static const char* endpoint = "ipc://bios-smtp-server-test";
+
+    // malamute broker
+    zactor_t *malamute = zactor_new (mlm_server, (void*) "Malamute");
+    assert (malamute);
+    if (verbose)
+        zstr_send (malamute, "VERBOSE");
+    zstr_sendx (malamute, "BIND", endpoint, NULL);
+
+    mlm_client_t *client = mlm_client_new ();
+    assert (client);
+
+    zmsg_t *message = NULL;
+    char *fullpath = NULL;
+    rt_t *data = rt_new ();
+
+    // --------------------------------------------------------------
+    FILE *fp = freopen ("stderr.txt", "w+", stderr);
+    // empty message - expected fail
+    message = zmsg_new ();
+    assert (message);
+    int rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);
+    // empty string - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "");   
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);  
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);
+    // unknown command - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "MAGIC!");   
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);  
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);
+    // CONFIGURE - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONFIGURE");
+    // missing config_file here
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);
+    // CONNECT - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONNECT");   
+    zmsg_addstr (message, endpoint);
+    // missing name here
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);
+    // CONNECT - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONNECT");   
+    // missing endpoint here
+    // missing name here
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);  
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);
+    // CONNECT - expected fail; bad endpoint
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONNECT");   
+    zmsg_addstr (message, "ipc://bios-smtp-server-BAD");
+    zmsg_addstr (message, "test-agent");
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);  
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);    
+    // CONSUMER - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONSUMER");
+    zmsg_addstr (message, "some-stream");   
+    // missing pattern here
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);    
+    // CONSUMER - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONSUMER");
+    // missing stream here
+    // missing pattern here
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);    
+    // PRODUCER - expected fail
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "PRODUCER");
+    // missing stream here
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    STDERR_NON_EMPTY
+
+    // The original client still waiting on the bad endpoint for malamute
+    // server to show up. Therefore we must destroy and create it again.
+    mlm_client_destroy (&client);
+    client = mlm_client_new ();
+    assert (client);
+
+    // --------------------------------------------------------------
+    fp = freopen ("stderr.txt", "w+", stderr);    
+
+    // $TERM
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "$TERM");   
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 1);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    // CONNECT
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONNECT");   
+    zmsg_addstr (message, endpoint);
+    zmsg_addstr (message, "test-agent");   
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    // CONSUMER
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONSUMER");
+    zmsg_addstr (message, "some-stream");   
+    zmsg_addstr (message, ".+@.+");   
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    // PRODUCER
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "PRODUCER");
+    zmsg_addstr (message, "some-stream");   
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath == NULL);
+
+    // CONFIGURE
+    message = zmsg_new ();
+    assert (message);
+    zmsg_addstr (message, "CONFIGURE");
+    zmsg_addstr (message, "./test_state_file");
+    rv = actor_commands (client, &message, data, &fullpath);
+    assert (rv == 0);
+    assert (message == NULL);
+    assert (fullpath);
+    assert (streq (fullpath,"./test_state_file"));
+    zstr_free (&fullpath);
+
+    STDERR_EMPTY
+
+    rt_destroy (&data);
+    zmsg_destroy (&message);
+    mlm_client_destroy (&client);
+    zactor_destroy (&malamute);
+
+    //  @end*/
+    printf ("OK\n");
+}
---
+++ src/data.cc
@@ -0,0 +1,337 @@
+/*  =========================================================================
+    data - composite metrics data structure
+
+    Copyright (C) 2014 - 2015 Eaton                                        
+                                                                           
+    This program is free software; you can redistribute it and/or modify   
+    it under the terms of the GNU General Public License as published by   
+    the Free Software Foundation; either version 2 of the License, or      
+    (at your option) any later version.                                    
+                                                                           
+    This program is distributed in the hope that it will be useful,        
+    but WITHOUT ANY WARRANTY; without even the implied warranty of         
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
+    GNU General Public License for more details.                           
+                                                                           
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    =========================================================================
+*/
+
+/*
+@header
+    data - composite metrics data structure
+@discuss
+@end
+*/
+
+#include "composite_metrics_classes.h"
+
+//  Structure of our class
+
+struct _data_t {
+    zhashx_t *assets;
+    char *state_file; 
+};
+
+
+//  --------------------------------------------------------------------------
+//  Create a new data
+
+data_t *
+data_new (void)
+{
+    data_t *self = (data_t *) zmalloc (sizeof (data_t));
+    assert (self);
+    //  Initialize class properties here
+    //  assets
+    self->assets = zhashx_new ();
+    zhashx_set_destructor (self->assets, (zhashx_destructor_fn *) bios_proto_destroy);
+    //  state_file
+    self->state_file = strdup (""); 
+    return self;
+}
+
+//  --------------------------------------------------------------------------
+//  Store asset 
+
+void
+data_asset_put (data_t *self, bios_proto_t **message_p)
+{
+    assert (self);
+    assert (message_p);
+    if (!*message_p)
+        return;
+    bios_proto_t *message = *message_p;
+    zhashx_update (self->assets, bios_proto_name (message), message);
+    *message_p = NULL;
+}
+
+//  --------------------------------------------------------------------------
+//  Get asset of specified name or NULL
+
+zlistx_t *
+data_asset_names (data_t *self)
+{
+    assert (self);
+    zlistx_t *list = zhashx_keys (self->assets);
+    zlistx_set_comparator (list, (czmq_comparator *) strcmp);
+    return list;
+}
+
+//  --------------------------------------------------------------------------
+//  Get asset of specified name or NULL
+
+bios_proto_t *
+data_asset (data_t *self, const char *name)
+{
+    assert (self);
+    assert (name);
+    return (bios_proto_t *) zhashx_lookup (self->assets, name);
+}
+
+//  --------------------------------------------------------------------------
+//  Get fullpath to state file or NULL
+
+const char *
+data_statefile (data_t *self)
+{
+    assert (self);
+    return self->state_file;
+}
+
+//  --------------------------------------------------------------------------
+//  Set fullpath to state file
+
+void
+data_set_statefile (data_t *self, const char *fullpath)
+{
+    assert (self);
+    assert (fullpath);
+    zstr_free (&self->state_file);
+    self->state_file = strdup (fullpath);
+}
+
+//  --------------------------------------------------------------------------
+//  Save data to disk
+//  0 - success, -1 - error
+
+int
+data_save (data_t *self)
+{
+    return 0;
+}
+
+//  --------------------------------------------------------------------------
+//  Load data from disk
+//  0 - success, -1 - error
+
+int
+data_load (data_t *self)
+{
+    return 0;
+}
+
+//  --------------------------------------------------------------------------
+//  Destroy the data
+
+void
+data_destroy (data_t **self_p)
+{
+    if (!self_p)
+        return;
+    if (*self_p) {
+        data_t *self = *self_p;
+        //  Free class properties here
+        zhashx_destroy (&self->assets);
+        zstr_free (&self->state_file);
+        //  Free object itself
+        free (self);
+        *self_p = NULL;
+    }
+}
+
+//  --------------------------------------------------------------------------
+//  Self test of this class
+
+//  Helper test function
+//  create new ASSET message of type bios_proto_t
+static bios_proto_t *
+test_asset_new (const char *name, const char *operation)
+{
+    assert (name);
+    assert (operation);
+
+    bios_proto_t *asset = bios_proto_new (BIOS_PROTO_ASSET);
+    bios_proto_set_name (asset, "%s", name);
+    bios_proto_set_operation (asset, "%s", operation);
+    return asset;
+}
+
+//  Helper test function
+//  0 - same, 1 - different  
+static int
+test_zlistx_compare (zlistx_t *expected, zlistx_t **received_p, int print = 0)
+{
+    assert (expected); 
+    assert (received_p && *received_p);
+
+    zlistx_t *received = *received_p;
+
+    if (print) {
+        zsys_debug ("received");
+        const char *first = (const char *) zlistx_first (received);
+        while (first) {
+            zsys_debug ("\t%s", first);
+            first = (const char *) zlistx_next (received);
+        }
+        zsys_debug ("expected");
+        first = (const char *) zlistx_first (expected);
+        while (first) {
+            zsys_debug ("\t%s", first);
+            first = (const char *) zlistx_next (expected);
+        }
+    }
+
+    int rv = 1;
+    const char *cursor = (const char *) zlistx_first (expected);
+    while (cursor) {
+        void *handle = zlistx_find (received, (void *) cursor);
+        if (!handle)
+            break;
+        zlistx_delete (received, handle);
+        cursor = (const char *) zlistx_next (expected);
+    }
+    if (zlistx_size (received) == 0)
+        rv = 0;
+    zlistx_destroy (received_p);
+    *received_p = NULL;
+    return rv;
+}
+
+void
+data_test (bool verbose)
+{
+    printf (" * data: ");
+
+    //  @selftest
+    //  Simple create/destroy test    
+    data_t *self = data_new ();
+    assert (self);
+    
+    data_destroy (&self);
+    assert (self == NULL);
+    
+    data_destroy (&self);
+    assert (self == NULL);
+    
+    self = data_new ();
+
+    // data_(set)_statefile 
+    const char *state_file = data_statefile (self);
+    assert (streq (state_file, ""));
+
+    data_set_statefile (self, "/home/kj/work/state_file");
+    
+    state_file = data_statefile (self);
+    assert (streq (state_file, "/home/kj/work/state_file"));
+
+    data_set_statefile (self, "./state_file");
+    
+    state_file = data_statefile (self);
+    assert (streq (state_file, "./state_file"));
+
+    // asset
+    zlistx_t *expected = zlistx_new ();
+    zlistx_set_destructor (expected, (czmq_destructor *) zstr_free);
+    zlistx_set_duplicator (expected, (czmq_duplicator *) strdup);
+    zlistx_set_comparator (expected, (czmq_comparator *) strcmp);
+
+    bios_proto_t *asset =  test_asset_new ("ups", BIOS_PROTO_ASSET_OP_CREATE);
+    bios_proto_aux_insert (asset, "type", "%s", "device");
+    bios_proto_aux_insert (asset, "subtype", "%s", "ups");
+
+    bios_proto_ext_insert (asset, "abc.d", "%s", " ups string 1");
+    data_asset_put (self, &asset);
+    zlistx_add_end (expected, (void *) "ups");
+
+    asset =  test_asset_new ("epdu", BIOS_PROTO_ASSET_OP_CREATE);
+    bios_proto_aux_insert (asset, "type", "%s", "device");
+    bios_proto_aux_insert (asset, "subtype", "%s", "epdu");
+    data_asset_put (self, &asset);
+    zlistx_add_end (expected, (void *) "epdu");
+
+    asset =  test_asset_new ("ROZ.UPS33", BIOS_PROTO_ASSET_OP_CREATE);
+    bios_proto_aux_insert (asset, "type", "%s", "device");
+    bios_proto_aux_insert (asset, "subtype", "%s", "ups");
+    bios_proto_aux_insert (asset, "parent", "%s", "4");
+    bios_proto_aux_insert (asset, "status", "%s", "active");
+    bios_proto_aux_insert (asset, "priority", "%s", "2");
+
+    bios_proto_ext_insert (asset, "d.ef", "%s", "roz.ups33 string 1");
+    bios_proto_ext_insert (asset, "description" , "%s",  "UPS1 9PX 5000i");
+    bios_proto_ext_insert (asset, "device.location" , "%s",  "New IT Power LAB");
+    bios_proto_ext_insert (asset, "location_u_pos" , "%s",  "1");
+    bios_proto_ext_insert (asset, "u_size" , "%s",  "3");
+    bios_proto_ext_insert (asset, "device.contact" , "%s",  "Gabriel Szabo");
+    bios_proto_ext_insert (asset, "hostname.1" , "%s",  "ups33.roz53.lab.etn.com");
+    bios_proto_ext_insert (asset, "battery.type" , "%s",  "PbAc");
+    bios_proto_ext_insert (asset, "phases.output" , "%s",  "1");
+    bios_proto_ext_insert (asset,  "device.type" , "%s",  "ups");
+    bios_proto_ext_insert (asset, "business_critical" , "%s",  "yes");
+    bios_proto_ext_insert (asset, "status.outlet.2" , "%s",  "on");
+    bios_proto_ext_insert (asset, "status.outlet.1" , "%s",  "on");
+    bios_proto_ext_insert (asset, "serial_no" , "%s",  "G202D51129");
+    bios_proto_ext_insert (asset, "ups.serial" , "%s",  "G202D51129");
+    bios_proto_ext_insert (asset, "installation_date" , "%s",  "2015-01-05");
+    bios_proto_ext_insert (asset, "model" , "%s",  "Eaton 9PX");
+    bios_proto_ext_insert (asset, "phases.input" , "%s",  "1");
+    bios_proto_ext_insert (asset, "ip.1" , "%s",  "10.130.53.33");
+    bios_proto_ext_insert (asset, "ups.alarm" , "%s",  "Automatic bypass mode!");
+    bios_proto_ext_insert (asset, "manufacturer" , "%s",  "EATON");
+    data_asset_put (self, &asset);
+    zlistx_add_end (expected, (void *) "ROZ.UPS33");
+
+    asset =  test_asset_new ("ROZ.ePDU14", BIOS_PROTO_ASSET_OP_CREATE);
+    bios_proto_aux_insert (asset, "type", "%s", "device");
+    bios_proto_aux_insert (asset, "subtype", "%s", "epdu");
+    bios_proto_aux_insert (asset, "parent", "%s", "4");
+    bios_proto_aux_insert (asset, "status", "%s", "active");
+    bios_proto_aux_insert (asset, "priority", "%s", "1"); 
+    data_asset_put (self, &asset);
+    zlistx_add_end (expected, (void *) "ROZ.ePDU14");
+
+    {
+        zlistx_t *received = data_asset_names (self);
+        assert (received);
+
+        int rv = test_zlistx_compare (expected, &received);
+        assert (rv == 0);
+
+        /*
+        assert (nut_asset_ip (self, "non-existing-asset") == NULL);
+        assert (nut_asset_daisychain (self, "non-existing-asset") == NULL);
+
+        assert (streq (nut_asset_ip (self, "ups"), ""));
+        assert (streq (nut_asset_daisychain (self, "ups"), ""));
+
+        assert (streq (nut_asset_ip (self, "epdu"), ""));
+        assert (streq (nut_asset_daisychain (self, "epdu"), ""));
+
+        assert (streq (nut_asset_ip (self, "ROZ.UPS33"), "10.130.53.33"));
+        assert (streq (nut_asset_daisychain (self, "ROZ.UPS33"), ""));
+
+        assert (streq (nut_asset_ip (self, "MBT.EPDU4"), "10.130.53.33"));
+        assert (streq (nut_asset_daisychain (self, "MBT.EPDU4"), "3"));
+
+        assert (streq (nut_asset_ip (self, "ROZ.ePDU14"), "10.130.53.33"));
+        assert (streq (nut_asset_daisychain (self, "ROZ.ePDU14"), "2"));
+        */
+    }
+
+    zlistx_destroy (&expected);
+    data_destroy (&self);
+    //  @end
+    printf ("OK\n");
+}
