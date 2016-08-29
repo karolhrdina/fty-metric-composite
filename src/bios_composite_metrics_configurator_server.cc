e1934a22600fdf21643fd9d633691634363853ac
Chernikava, Alena <AlenaChernikava@Eaton.com>
2016-08-29 13:55:03 +0200

    problem: default value is true Solution: state it in the _new()
    Signed-off-by: Chernikava, Alena <AlenaChernikava@Eaton.com>

--- src/bios_composite_metrics_configurator_server.cc
+++ src/bios_composite_metrics_configurator_server.cc
@@ -323,6 +323,7 @@ s_regenerate (c_metric_conf_t *cfg, std::set <std::string> &metrics_unavailable)
         log_error ("data_asset_names () failed");
         return;
     }
+    log_debug ("propagation: %s",  cfg->is_propagation_needed ? "true": "false");
     data_reassign_sensors (data, cfg->is_propagation_needed);
     log_info ("New configuration was deduced");
     const char *asset = (const char *) zlistx_first (assets);
